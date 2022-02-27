bd0179dc94f2eb4634b2a7f7df91eb68f428d291
Dien-ES <dien.eaststar@gmail.com>
2022-02-27 18:05:42 +0900

    Add amplitude parameter of FallRisk_classification

--- FallRisk_classification.py
+++ FallRisk_classification.py
@@ -3,7 +3,7 @@ from EMG_analysis import *
 COL_IDX = [2, 3, 4, 5, 8, 9, 10, 11]
 
 
-def data_helper(params, mv_list):
+def data_helper(params, mv_list, is_amp=False):
     total_df = pd.DataFrame()
     for idx, mv in enumerate(mv_list):
         df1 = pd.DataFrame()
@@ -16,20 +16,29 @@ def data_helper(params, mv_list):
                 submvc = list(np.nanmax(param.submvc.data[:, COL_IDX],
                                         axis=0))
 
-                rms_amp = list(param.rms_amp[:, COL_IDX].reshape(-1))
-                mvc_amp = list(param.mvc_amp[:, COL_IDX].reshape(-1))
-                submvc_amp = list(param.submvc_amp[:, COL_IDX].reshape(-1))
+                if is_amp:
+                    rms_amp = list(param.rms_amp[:, COL_IDX].reshape(-1))
+                    mvc_amp = list(param.mvc_amp[:, COL_IDX].reshape(-1))
+                    submvc_amp = list(param.submvc_amp[:, COL_IDX].reshape(-1))
 
-                df2 = pd.DataFrame(group +
-                                   rms + mvc + submvc +
-                                   rms_amp + mvc_amp + submvc_amp).T
-                df2.columns = ['Group'] + \
-                      [f'RMS/{c}' for c in np.array(COLUMNS)[COL_IDX]] + \
-                      [f'MVC/{c}' for c in np.array(COLUMNS)[COL_IDX]] + \
-                      [f'subMVC/{c}' for c in np.array(COLUMNS)[COL_IDX]] + \
-                      amp_colnames('RMS') +\
-                      amp_colnames('MVC') +\
-                      amp_colnames('subMVC')
+                    df2 = pd.DataFrame(group +
+                                       rms + mvc + submvc +
+                                       rms_amp + mvc_amp + submvc_amp).T
+                    df2.columns = ['Group'] + \
+                          [f'RMS/{c}' for c in np.array(COLUMNS)[COL_IDX]] + \
+                          [f'MVC/{c}' for c in np.array(COLUMNS)[COL_IDX]] + \
+                          [f'subMVC/{c}' for c in np.array(COLUMNS)[COL_IDX]] + \
+                          amp_colnames('RMS') +\
+                          amp_colnames('MVC') +\
+                          amp_colnames('subMVC')
+                else:
+                    df2 = pd.DataFrame(group +
+                                       rms + mvc + submvc).T
+                    df2.columns = ['Group'] + \
+                          [f'RMS/{c}' for c in np.array(COLUMNS)[COL_IDX]] + \
+                          [f'MVC/{c}' for c in np.array(COLUMNS)[COL_IDX]] + \
+                          [f'subMVC/{c}' for c in np.array(COLUMNS)[COL_IDX]]
+
                 df1 = pd.concat([df1, df2])
 
         df1['Movement'] = mv
